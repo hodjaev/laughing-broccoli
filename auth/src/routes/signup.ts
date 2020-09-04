dadb97a8fd48e20c384e4aa2abc3e590031ca805
Bahram Hodjaev <bahram-hodjaev@yandex.ru>
2020-09-04 20:42:40 +0300

    Add in custom error handler

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
 router.post(
   '/api/users/signup',
   [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
+    body('email').isEmail().withMessage('Email must be valid'),
     body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
@@ -18,10 +18,12 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     console.log('Creating a user');
+    throw new DatabaseConnectionError();
+
     res.send({});
   }
 );
