180c843d484c08267029759ba445a48650de9136
Bahram Hodjaev <bahram-hodjaev@yandex.ru>
2020-09-12 23:13:27 +0300

    Add invalid input test

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,48 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test',
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      pasword: 'p'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      pasword: 'password'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({})
+    .expect(400);
+});
