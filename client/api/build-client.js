8b0a1e6a9569ceafe19bdf331a920945253ef121
Bahram Hodjaev <bahram-hodjaev@yandex.ru>
2020-09-24 23:16:39 +0300

    Update ingress internal url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://ingress-nginx.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://ingress-nginx.ingress-nginx-controller.svc.cluster.local',
       headers: req.headers
     });
   }
